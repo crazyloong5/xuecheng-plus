e2ac9dec44adb5b84b0a1f2210343ee1e0ac8ac1
crazyloong5 <2603327054@qq.com>
2024-12-23 20:48:37 +0800

    修改课程

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -4,25 +4,40 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.AddCourseDto;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
 public interface CourseBaseInfoService  {
 
- /**
-  * @description 课程查询接口
-  * @param pageParams 分页参数
-  * @param queryCourseParamsDto 条件条件
-  * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
- */
-  PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+    /**
+     * @description 课程查询接口
+     * @param pageParams 分页参数
+     * @param queryCourseParamsDto 条件条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+    */
+     PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
 
-   /**
-  * @description 添加课程基本信息
-  * @param companyId  教学机构id
-  * @param addCourseDto  课程基本信息
-  * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
- */
-  CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+      /**
+     * @description 添加课程基本信息
+     * @param companyId  教学机构id
+     * @param addCourseDto  课程基本信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+    */
+     CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
 
+     /**
+     * @description 课程查询接口
+     * @param courseId 课程id
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+    */
+     CourseBaseInfoDto getCourseBaseInfo(Long courseId);
+
+     /**
+      * @description 修改课程信息
+      * @param companyId  机构id
+      * @param dto  课程信息
+      * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     */
+     CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto dto);
 }
