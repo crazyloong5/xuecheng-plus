d8af4127eb2b03c90215b3810e128b92abb0888c
crazyloong5 <2603327054@qq.com>
2024-12-23 20:14:33 +0800

    JSR303校验

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,12 @@
+package com.xuecheng.base.exception;
+ /**
+ * @description 校验分组
+ * @version 1.0
+ */
+public class ValidationGroups {
+
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+
+}
