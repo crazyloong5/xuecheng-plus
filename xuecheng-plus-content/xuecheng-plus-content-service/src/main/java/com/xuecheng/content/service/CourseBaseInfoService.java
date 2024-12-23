3ce14b7d0d506967411a6375c3ec82e75e78a63c
crazyloong5 <2603327054@qq.com>
2024-12-23 17:57:53 +0800

    新增课程接口实现

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -2,12 +2,14 @@ package com.xuecheng.content.service;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
 public interface CourseBaseInfoService  {
 
- /*
+ /**
   * @description 课程查询接口
   * @param pageParams 分页参数
   * @param queryCourseParamsDto 条件条件
@@ -15,4 +17,12 @@ public interface CourseBaseInfoService  {
  */
   PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
 
- }
+   /**
+  * @description 添加课程基本信息
+  * @param companyId  教学机构id
+  * @param addCourseDto  课程基本信息
+  * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+ */
+  CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+
+}
